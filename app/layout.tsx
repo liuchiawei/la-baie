e4b48719844f067548b80ebcd7542589abb80714
LIU CHIAWEI <doublecheap@gmail.com>
2026-02-25 15:41:48 +0900

    Fix typo in className attribute

--- app/layout.tsx
+++ app/layout.tsx
@@ -53,7 +53,7 @@ export default function RootLayout({
       <body
         className={`${notoSerifJP.variable} ${cormorant.variable} ${roboto.variable} antialiased bg-neutral-950 text-foreground`}
       >
-        <div claseeName="w-full h-full mx-auto bg-neutral-950 text-neutral-50">
+        <div className="w-full h-full mx-auto bg-neutral-950 text-neutral-50">
           <ConditionalHeader />
           {children}
           <Footer />
